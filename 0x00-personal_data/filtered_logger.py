a8b9873c3439a9a3313c1fc5791f710f0ead3888
Andee WSL <andeeolayinkascott@gmail.com>
2024-01-14 17:34:03 +0100

    Task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -41,7 +41,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List[str]) -> None:
+    def __init__(self, fields: List[str]):
         """"""
         self.fields: List[str] = fields
         super(RedactingFormatter, self).__init__(self.FORMAT)
