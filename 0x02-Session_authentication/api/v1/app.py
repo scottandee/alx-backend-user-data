8676abdc56af3619464b401e9dcf5fcf8cad5319
Andee WSL <andeeolayinkascott@gmail.com>
2024-01-17 13:41:27 +0100

    fix

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -59,8 +59,8 @@ def not_found(error) -> str:
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    auth = getenv("AUTH_TYPE")
-    if auth == "basic_auth":
+    authentication = getenv("AUTH_TYPE")
+    if authentication == "basic_auth":
         from api.v1.auth.basic_auth import BasicAuth
         auth = BasicAuth()
     else:
