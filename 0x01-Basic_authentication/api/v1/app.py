d7180781a9c1787351f9a450afb514fb22b45cbf
Andee WSL <andeeolayinkascott@gmail.com>
2024-01-17 13:28:01 +0100

    Task 6 fix

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -59,12 +59,13 @@ def not_found(error) -> str:
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    auth = getenv("AUTH_TYPE")
-    if auth == "basic_auth":
+    authentication = getenv("AUTH_TYPE")
+    if authentication == "basic_auth":
         from api.v1.auth.basic_auth import BasicAuth
         auth = BasicAuth()
     else:
         from api.v1.auth.auth import Auth
         auth = Auth()
+
 
     app.run(host=host, port=port, debug=True)
