120660b4e816125f9c719626744d028f4dc8456b
Andee WSL <andeeolayinkascott@gmail.com>
2024-01-17 05:42:26 +0100

    Task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,7 @@ Basic Authentication module
 
 from api.v1.auth.auth import Auth
 import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -19,7 +20,7 @@ class BasicAuth(Auth):
 
         Returns:
           - None if authorization header is none, not a string, or if
-            decoded string doesn't start with Basic
+            string doesn't start with Basic
           - The value after "Basic"
         """
         if authorization_header is None:
@@ -29,3 +30,24 @@ class BasicAuth(Auth):
         if authorization_header[:6] != "Basic ":
             return
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str
+            ) -> str:
+        """ decode_base64_authorization_header
+        decode the authorization header with base64
+
+        Returns:
+          - None if base64_authorization_header is None,
+            not a string, or is not valid base64
+          - The decoded value as UTF8 string
+        """
+        if base64_authorization_header is None:
+            return
+        if not isinstance(base64_authorization_header, str):
+            return
+        try:
+            decoded = base64.b64decode(base64_authorization_header)
+            return decoded.decode("utf-8")
+        except binascii.Error:
+            return
