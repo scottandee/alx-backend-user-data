a69d1eb1b61922b8612f26b68e5ccc76105b2b5f
Andee WSL <andeeolayinkascott@gmail.com>
2024-01-18 03:19:18 +0100

    Task 6

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@ Session Authentication module
 """
 
 from api.v1.auth.auth import Auth
+from models.user import User
 from uuid import uuid4
 
 
@@ -37,3 +38,15 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return
         return SessionAuth.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ current_user
+        Defines the current user
+
+        Returns:
+          - current user
+          - None
+        """
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        return User.get(user_id)
